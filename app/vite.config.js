534584fe7b38896f4a5c5550b4cd04f1a56f9241
gonzalorportfolio <gromeroeducation@gmail.com>
2025-01-23 12:09:37 -0500

    check

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,6 +1,16 @@
-import { defineConfig } from 'vite'
-
+import { defineConfig } from "vite";
 export default defineConfig({
-  build: { outDir: '..' },
-  base: '/poke-a-view-day-five/', 
+  // GitHub Pages expects an index.html in the root directory
+  // so just run npm build before pushing to GitHub and this will rebuild our assets to the root
+  build: {
+    outDir: "..",
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        page1: "./src/other.html",
+      },
+    },
+  },
+  // needed for github pages just put the repo name here
+  base: "/poke-a-view-day-five/",
 });
